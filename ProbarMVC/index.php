0b37f9464ad12aee19a821c691f1d095f49b685f
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2025-11-26 19:20:39 +0100

    Update index.php

--- ProbarMVC/index.php
+++ ProbarMVC/index.php
@@ -1,3 +1,7 @@
 <?php
+/*El index esta creado para que cuando me ejecute me llame al controlador de la vista del formulario por no tener
+que estar todo el rato clicando al controlador del formulario para poder llamar a la vista
+En resumen esta realizado sin index */
     require_once __DIR__ . '/controlador/cFormulario.php';
+
 ?>
