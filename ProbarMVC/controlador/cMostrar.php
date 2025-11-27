62d373463b09cdfc8f5881d1db67c978edcf7aff
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2025-11-27 14:40:58 +0100

    Delete ProbarMVC directory

--- ProbarMVC/controlador/cMostrar.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../modelo/Mboletin_Usuario.php';
-
-$usuario = new Boletin_Usuario();
-$listaUsuarios = $usuario->sacarUsuarios();
-
-require_once __DIR__ . '/../vista/monstrarModificarBorrar.php';
-?>
